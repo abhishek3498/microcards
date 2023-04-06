bd61a9fff92d1b9e568ed353f3c876629a8adee2
abhishek3498 <67725095+abhishek3498@users.noreply.github.com>
2023-04-06 11:06:40 +0530

    Add files via upload

---
+++ cards/src/main/java/com/Cards/controller/CardsController.java
@@ -0,0 +1,31 @@
+package com.Cards.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.Cards.model.Cards;
+import com.Cards.model.Customer;
+import com.Cards.repository.CardsRepository;
+
+@RestController
+public class CardsController {
+
+	@Autowired
+	private CardsRepository cardsRepository;
+
+	@PostMapping("/myCards")
+	public List<Cards> getCardDetails(@RequestBody Customer customer) {
+		List<Cards> cards = cardsRepository.findByCustomerId(customer.getCustomerId());
+		if (cards != null) {
+			return cards;
+		} else {
+			return null;
+		}
+
+	}
+
+}
